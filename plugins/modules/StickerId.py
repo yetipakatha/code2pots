fb3fc37059fa19146b730f797249df5ef511b829
abitha000 <abithak000@yahoo.com>
2024-09-04 22:51:08 +0530

    Update StickerId.py

--- plugins/modules/StickerId.py
+++ plugins/modules/StickerId.py
@@ -13,8 +13,8 @@ from pyrogram.types import User, Message, Sticker, Document
 bughunter0 = Client(
     "Sticker-Bot",
     bot_token = os.environ.get("BOT_TOKEN"),
-    api_id = int(os.environ.get("API_ID")),
-    api_hash = os.environ.get("API_HASH")
+    api_id = int(os.environ.get('API_ID')),
+    api_hash = os.environ.get('API_HASH')
 )
 
 START_STRING = """ Hi {}, I'm Sticker Bot. 
